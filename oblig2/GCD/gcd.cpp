d4efd4513a93eaa711821646f3195dde0a8e3d04
fruithut <fruithut@tutanota.com>
2018-10-08 20:38:10 +0200

    [gcd.cpp] Corrected gdc() method behaviour.

--- oblig2/GCD/gcd.cpp
+++ oblig2/GCD/gcd.cpp
@@ -4,16 +4,21 @@
 int gcd(int n, int m) {
     A<int> x = n;
     A<int> y = m;
-    while (n != m) {
+    while (x != y) {
       CO([&]{ ATO if (x > y)  x = x - y; MIC; },
          [&]{ ATO if (x < y)  y = y - x; MIC; });
     }
 
+    logl("GCD OF: ", n, " & ", m, " = ", x);
     return x;
 }
 
 int main() {
-  logl(gcd(2, 3));
-  logl(gcd(25235, 2345));
-  logl(gcd(310, 150));
+    // test a few results
+    int result1 = gcd(2,3);
+    assert(result1 == 1);
+    int result2 = gcd(25235, 2345);
+    assert(result2 == 35);
+    int result3 = gcd(310, 150);
+    assert(result3 == 10);
 }
